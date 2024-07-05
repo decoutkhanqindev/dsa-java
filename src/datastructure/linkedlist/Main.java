cdc71477340fa21f2e6808395f6d32e2889920fa
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-05 16:42:34 +0700

    linked list: using getter() and setter()

--- src/datastructure/linkedlist/Main.java
+++ src/datastructure/linkedlist/Main.java
@@ -2,6 +2,10 @@ package datastructure.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
 
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
     }
 }
