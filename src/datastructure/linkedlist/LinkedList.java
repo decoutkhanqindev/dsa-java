c452485abd4e63992990dd2ff33d4e6fb9df59be
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-05 16:37:54 +0700

    linked list: create constructor

--- src/datastructure/linkedlist/LinkedList.java
+++ src/datastructure/linkedlist/LinkedList.java
@@ -1,5 +1,14 @@
 package datastructure.linkedlist;
 
 public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
 
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
 }
