6c0326056fb15272ca26a7f926732907b99159ac
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-05 20:53:27 +0700

    linked list: using removeLast()

--- src/datastructure/linkedlist/Main.java
+++ src/datastructure/linkedlist/Main.java
@@ -3,8 +3,9 @@ package datastructure.linkedlist;
 public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
-
         myLinkedList.append(5);
+
+        myLinkedList.removeLast();
         myLinkedList.printList();
     }
 }
