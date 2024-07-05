6bb53f989e49fa61445f23923899f4a126358658
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-05 17:11:05 +0700

    linked list: using append()

--- src/datastructure/linkedlist/Main.java
+++ src/datastructure/linkedlist/Main.java
@@ -4,12 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
         myLinkedList.append(5);
-
         myLinkedList.printList();
     }
 }
