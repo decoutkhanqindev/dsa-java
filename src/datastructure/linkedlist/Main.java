84eb493953b00dce07226c1cfacbf1d5f681cdc0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-06 21:51:48 +0700

    linked list: using get()

--- src/datastructure/linkedlist/Main.java
+++ src/datastructure/linkedlist/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
         myLinkedList.append(5);
+        myLinkedList.append(10);
+        myLinkedList.append(9);
+        myLinkedList.append(13);
+        myLinkedList.append(8);
         myLinkedList.prepend(1);
 //        myLinkedList.removeLast();
-        myLinkedList.removeFirst();
+//        myLinkedList.removeFirst();
         myLinkedList.printList();
+
+        System.out.println(myLinkedList.get(4).getValue());
     }
 }
