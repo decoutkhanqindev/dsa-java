4890da605ff4c451fa43a7a9ca76ca0b233c37db
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-06 22:25:04 +0700

    linked list: create insert()

--- src/datastructure/linkedlist/LinkedList.java
+++ src/datastructure/linkedlist/LinkedList.java
@@ -124,4 +124,22 @@ public class LinkedList {
         }
         return false;
     }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.setNext(temp.getNext());
+        temp.setNext(newNode);
+        length++;
+        return true;
+    }
 }
