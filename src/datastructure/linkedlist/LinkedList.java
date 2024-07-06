b74c197e9aca0b1150c6bfd486a77e4c658038f0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-06 22:56:48 +0700

    linked list: create remove()

--- src/datastructure/linkedlist/LinkedList.java
+++ src/datastructure/linkedlist/LinkedList.java
@@ -142,4 +142,16 @@ public class LinkedList {
         length++;
         return true;
     }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == (length - 1)) return removeLast();
+        Node prev = get(index - 1);
+        Node temp = prev.getNext();
+        prev.setNext(temp.getNext());
+        temp.setNext(null);
+        length--;
+        return temp;
+    }
 }
