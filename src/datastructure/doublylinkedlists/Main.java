36c9ef4e242b6c93fd6e86ba3bb300e7feb2eccc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-07 22:45:56 +0700

    doubly linked list: using append()

--- src/datastructure/doublylinkedlists/Main.java
+++ src/datastructure/doublylinkedlists/Main.java
@@ -3,10 +3,10 @@ package datastructure.doublylinkedlists;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
-
+        doublyLinkedList.append(10);
         doublyLinkedList.getHead();
-        doublyLinkedList.getTail();
-        doublyLinkedList.getLength();
+//        doublyLinkedList.getTail();
+//        doublyLinkedList.getLength();
 
         doublyLinkedList.printList();
     }
