2a0100a014143641d27aafcb23553bd55961f4e9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-07 23:34:51 +0700

    doubly linked list: using prepend()

--- src/datastructure/doublylinkedlists/Main.java
+++ src/datastructure/doublylinkedlists/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
         doublyLinkedList.append(10);
-        doublyLinkedList.printList();
-
-        System.out.println(doublyLinkedList.removeLast().getValue());
+        doublyLinkedList.prepend(9);
         doublyLinkedList.printList();
     }
 }
