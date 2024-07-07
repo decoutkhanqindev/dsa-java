55459810a9a4f768c5e670dc0e0dcd65e0deddb1
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-07 22:28:50 +0700

    doubly linked list: create getter(), setter() and printList()

--- src/datastructure/doublylinkedlists/DoublyLinkedList.java
+++ src/datastructure/doublylinkedlists/DoublyLinkedList.java
@@ -1,4 +1,48 @@
 package datastructure.doublylinkedlists;
 
 public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public void getTail() {
+        if (tail != null) {
+            System.out.println("Tail: " + tail.getValue());
+        } else {
+            System.out.println("Tail: null");
+        }
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void printList() {
+        System.out.print("My Linked List: ");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.getValue() + " -> ");
+            temp = temp.getNext();
+        }
+        System.out.println("null");
+    }
 }
