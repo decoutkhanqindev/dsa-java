4d9145caaa75f366d2cecf747474fb97524da2d6
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-08 21:30:51 +0700

    doubly linked list: using insert()

--- src/datastructure/doublylinkedlists/Main.java
+++ src/datastructure/doublylinkedlists/Main.java
@@ -2,14 +2,10 @@ package datastructure.doublylinkedlists;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
-        doublyLinkedList.append(10);
-        doublyLinkedList.prepend(9);
-        doublyLinkedList.append(10);
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(1);
+        doublyLinkedList.append(2);
+        doublyLinkedList.insert(2, 3);
 
-        doublyLinkedList.printList();
-
-        doublyLinkedList.set(1, 18);
         doublyLinkedList.printList();
 
     }
