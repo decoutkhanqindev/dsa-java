9cde8affcb6dfad748171f35910f1ff38b9c6f8e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-08 22:03:16 +0700

    doubly linked list: using remove()

--- src/datastructure/doublylinkedlists/Main.java
+++ src/datastructure/doublylinkedlists/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(1);
         doublyLinkedList.append(2);
-        doublyLinkedList.insert(2, 3);
+        doublyLinkedList.append(3);
+        doublyLinkedList.append(4);
+        doublyLinkedList.append(10);
+        doublyLinkedList.append(9);
 
+        doublyLinkedList.remove(3);
         doublyLinkedList.printList();
 
     }
